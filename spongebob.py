021285e35d6d378e0718846c8aa3dc965455979c
sminix <48739178+sminix@users.noreply.github.com>
2020-06-26 20:54:50 -0500

    Remove input call in this file

--- spongebob.py
+++ spongebob.py
@@ -6,25 +6,24 @@ Sam Minix
 import random
 lower = 'abcdefghijklmnopqrstuvwxyz'
 upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-def spongebob(text, count):
-    if count == 250:
-        return text #return input if 50 recursions
+def spongebob(text):
     newText = '' #initialize new text
     choice = [True, False] #initialize choices
+    
     for i in range(len(text)): #for each letter in text
         new = text[i]
-        if new in lower: #if the new letter is lowercase
+        
+        if new in lower: #if the new letter is lowercase, radomly make uppercase
+            
             if random.choice(choice): #if the random choice is True
                 new = upper[lower.index(new)] #change to upper case letter
-        elif new in upper: #same if uppercase
-            if random.choice(choice):
-                new = lower[upper.index(new)]
+                
+        elif new in upper: #if uppercase, make lowercase
+            new = lower[upper.index(new)]
+            
         newText += new #add new letter to new text
-    #print(newText)
-    return spongebob(newText, count + 1) #recursive call to help randomize
-
-
-print(spongebob('Who lives in a Pineapple Under the Sea', 0))
+        
+    return newText 
                 
                 
             
